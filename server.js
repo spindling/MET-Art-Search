61a21a1783eca3215fe4e71286c5ab6bf92d3d49
spindling <jeremy.sean009@gmail.com>
2026-02-22 11:18:09 -0500

    Implemented working front-end to back-end call using preset query in script. API successfully called using q: sunflowers

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.get("/app", function(req,res) {
 
 });
 
+
+app.post("/METRequest", async function(req,res){
+
+  const response = await axios.get("https://collectionapi.metmuseum.org/public/collection/v1/search",
+    {params: {q: req.body.q}});
+  
+    res.json(
+      {
+        "total": response.data.total,
+
+      }
+    )
+  
+
+});
+
 app.listen(3000, function() {
     console.log("Server is listening on port 3000...");
 });

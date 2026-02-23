42f88882af32cd8ad65177e25dc1e910abb874da
spindling <jeremy.sean009@gmail.com>
2026-02-22 20:34:16 -0500

    Added artwork medium to search query. Updated to medium radio button ID's to match correct parameter string values to simplify API call

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.get("/app", function(req,res) {
 app.post("/METRequest", async function(req,res){
 
   const response = await axios.get("https://collectionapi.metmuseum.org/public/collection/v1/search",
-    {params: {q: req.body.q, isHighlight: req.body.isHighlight}});
+    {params: {q: req.body.q, isHighlight: req.body.isHighlight, medium: req.body.medium}});
   
     res.json(
       {
         "total": response.data.total,
         "objectIDs": response.data.objectIDs
+        
         
       }
     )
